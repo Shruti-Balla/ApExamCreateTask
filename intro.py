2fd2ee3a7da7f5b76f0e6c5bcf306f1eed4e05ad
PixelAI <44213124+PixelAI@users.noreply.github.com>
2021-04-16 09:48:52 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,15 +1,18 @@
-import os
+import turtle
 import sys
 from time import sleep
+from easyTypeWriter import typeWriter
+import winsound
 
-words = "Hello! Welcome to our typing game!"
-def clearScreen():
-  os.system("clear")
-
+#obj = typeWriter.EasyInput()
+words = "Hello! Welcome to our typing game! :P"
+'''
+obj.setEnterAudioPath("C:/Users/astro/Downloads/word_game_AP.py/ding3.wav")
+obj.setKeyboardAudioPath("C:/Users/astro/Downloads/word_game_AP.py/keyhee.wav")
+x = obj.takeInput(True , words)
+'''
 for char in words:
-    sleep(0.1)
+    sleep(0)
     sys.stdout.write(char)
     sys.stdout.flush()
-
-sleep(1)
-clearScreen()
+    winsound.PlaySound('keyhee.wav', winsound.SND_FILENAME)
