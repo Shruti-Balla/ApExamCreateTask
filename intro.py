1b8fa81cdb7538ec4b8ac7fb68de26282477813f
smohan979 <74677669+smohan979@users.noreply.github.com>
2021-04-16 09:05:00 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,1 +1,9 @@
 import turtle
+import sys
+from time import sleep
+
+words = "Hello! Welcome to our typing game! :P"
+for char in words:
+    sleep(0.2)
+    sys.stdout.write(char)
+    sys.stdout.flush()
