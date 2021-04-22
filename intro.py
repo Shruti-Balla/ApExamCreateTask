729dacf8a334d50833f0d13c672d76334d1127e8
PixelAI <44213124+PixelAI@users.noreply.github.com>
2021-04-22 09:24:51 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,5 +1,5 @@
 import turtle
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 import sys
 from time import sleep
 from easyTypeWriter import typeWriter
@@ -7,11 +7,12 @@ import winsound
 
 wn = turtle.Screen()
 wn.setup(600,600)
-wn.bgpic('sky.gif')
+sky =('sky.gif')
+wn.bgpic(sky)
 # message = "Hello"
 
 #obj = typeWriter.EasyInput()
-words = "Hello! Welcome to our typing game! :P"
+#swords = "Hello! Welcome to our typing game! :P"
 '''
 obj.setEnterAudioPath("C:/Users/astro/Downloads/word_game_AP.py/ding3.wav")
 obj.setKeyboardAudioPath("C:/Users/astro/Downloads/word_game_AP.py/keyhee.wav")
