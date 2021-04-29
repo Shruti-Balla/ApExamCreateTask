b83bf8d4bdf8bf9fa2a50cfb607b42edcdd3890b
Shruti-Balla <73190668+Shruti-Balla@users.noreply.github.com>
2021-04-29 09:33:41 -0400

    Update typing.py

--- typing.py
+++ typing.py
@@ -15,7 +15,7 @@ index = 0
 words = "Hello! Welcome to our typing game!"
 
 turtle.penup()
-turtle.goto(-200, 0)
+turtle.goto(-150, 30)
 
 for char in words:
     index += 1
@@ -25,7 +25,7 @@ for char in words:
     turtle.penup()
     turtle.goto(turtle.xcor() + 20, turtle.ycor())
     if index == 14:
-      turtle.goto(-275,-50)
+      turtle.goto(-225, -20)
     sys.stdout.flush()
 
 sleep(1)
@@ -35,7 +35,7 @@ index = 0
 words = "Instructions: type as many words as accurately as you can before the timer runs out. Good luck and have fun!"
 
 turtle.penup()
-turtle.goto(-200, 0)
+turtle.goto(-200, 90)
 
 for char in words:
     index += 1
@@ -45,13 +45,13 @@ for char in words:
     turtle.penup()
     turtle.goto(turtle.xcor() + 20, turtle.ycor())
     if index == 19:
-      turtle.goto(-275,-50)
+      turtle.goto(-275,40)
     if index == 47:
-      turtle.goto(-275,-100)
+      turtle.goto(-275,-10)
     if index == 75:
-      turtle.goto(-275,-150)
+      turtle.goto(-275,-60)
     if index == 103:
-      turtle.goto(-60,-200)
+      turtle.goto(-60,-110)
     sys.stdout.flush()
 
 sleep(1)
